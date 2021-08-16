9c4a63c0fdd805b9f731e80edd26d882861681cc
안서진 <ser0805@gmail.com>
2021-08-17 07:32:27 +0900

    Add: basic index.js

--- sprint1/basic/index.js
+++ sprint1/basic/index.js
@@ -1,5 +1,35 @@
 const Process = function () {
+    const input = "17sa8b";
+    const numInputs = input.replace(/[^0-9]/g, '').split('');
+    let answer = [];
+    let numbers = '';
+    let letters = '';
 
+    for (let i = 0; i < input.length; i++) {
+        if (numInputs.includes(input[i])) {
+            numbers += input[i];
+            if (i === input.length - 1) { // 마지막이라면
+                answer.push({ type: 'number', value: parseInt(numbers, 10) });
+                numbers = '';
+            }
+            if (!numInputs.includes(input[i + 1])) { // 다음 것이 문자라면
+                answer.push({ type: 'number', value: parseInt(numbers, 10) });
+                numbers = '';
+            }
+        }
+        else {
+            letters += input[i];
+            if (i === input.length - 1) { // 마지막이라면
+                answer.push({ type: 'string', value: letters });
+                numbers = '';
+            }
+            if (numInputs.includes(input[i + 1])) { // 다음 것이 숫자라면
+                answer.push({ type: 'string', value: letters });
+                letters = '';
+            }
+        }
+    }
+    console.log(answer);
 }
 
 Process();
