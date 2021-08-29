e97936ea8039bf37a363a919512e518c5d23d141
안서진 <ser0805@gmail.com>
2021-08-30 01:03:24 +0900

    Refactor: sprint1 basic mission 기능을 함수로 분리함

--- sprint1/basic/index.js
+++ sprint1/basic/index.js
@@ -1,30 +1,40 @@
-const Process = function () {
-    const input = "17sa8b";
+let answer = [];
+
+const pushToAnswer = (typeOfInput, valueOfInput) => {
+    if (typeOfInput == 'number') {
+        answer.push({ type: typeOfInput, value: parseInt(valueOfInput, 10) });
+    }
+    else {
+        answer.push({ type: typeOfInput, value: valueOfInput });
+    }
+}
+
+const isNumberInput = (input, i) => {
     const numInputs = input.replace(/[^0-9]/g, '').split('');
-    let answer = [];
+    return numInputs.includes(input[i])
+}
+
+const isLastInput = (input, i) => {
+    return i === input.length - 1
+}
+
+const Process = () => {
+    const input = "17sa8b";
     let numbers = '';
     let letters = '';
 
     for (let i = 0; i < input.length; i++) {
-        if (numInputs.includes(input[i])) {
+        if (isNumberInput(input, i)) {
             numbers += input[i];
-            if (i === input.length - 1) { // 마지막이라면
-                answer.push({ type: 'number', value: parseInt(numbers, 10) });
-                numbers = '';
-            }
-            if (!numInputs.includes(input[i + 1])) { // 다음 것이 문자라면
-                answer.push({ type: 'number', value: parseInt(numbers, 10) });
+            if (isLastInput(input, i) || !isNumberInput(input, i + 1)) { // 마지막이라면
+                pushToAnswer('number', numbers);
                 numbers = '';
             }
         }
         else {
             letters += input[i];
-            if (i === input.length - 1) { // 마지막이라면
-                answer.push({ type: 'string', value: letters });
-                numbers = '';
-            }
-            if (numInputs.includes(input[i + 1])) { // 다음 것이 숫자라면
-                answer.push({ type: 'string', value: letters });
+            if (isLastInput(input, i) || isNumberInput(input, i + 1)) { // 마지막이라면
+                pushToAnswer('string', letters);
                 letters = '';
             }
         }
